80ec08f6e1187244c23cbd21c8f2a4ea0f2d7659
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2015-06-20 01:24:44 +0530

    Added Zoom in/out on spatial domain image

--- fredo/editor/main.py
+++ fredo/editor/main.py
@@ -12,7 +12,13 @@ class EditorMainWindow(QMainWindow):
         super(EditorMainWindow, self).__init__(parent)
         self.ui = Ui_EditorMainWindow()
         self.ui.setupUi(self)
+
         self.ui.action_open.triggered.connect(self.open_file)
+        self.ui.image_zoom_in_btn.clicked.connect(self.image_zoom_in)
+        self.ui.image_zoom_out_btn.clicked.connect(self.image_zoom_out)
+
+        self.spatial_array = None
+        self.scale = 1.0
 
     def open_file(self):
         """ Signal handler for the Open Menu """
@@ -28,6 +34,7 @@ class EditorMainWindow(QMainWindow):
 
             array = util.qimage_to_numpy(image)
             garray = util.rgb_to_gray(array)
+            self.spatial_array = garray
             self.set_gray_image(garray)
 
     def set_gray_image(self, gimg):
@@ -35,7 +42,29 @@ class EditorMainWindow(QMainWindow):
 
         img = util.gray_to_rgb(gimg)
         qimage = util.numpy_to_qimage(img)
-        self.ui.image_label.setPixmap(QPixmap.fromImage(qimage))
+        w, h = qimage.width(), qimage.height()
+        sw, sh = int(w*self.scale), int(h*self.scale)
+        pixmap = QPixmap.fromImage(qimage)
+        scaled_pixmap = pixmap.scaled(sw, sh)
+        self.ui.image_label.setPixmap(scaled_pixmap)
+
+    def image_zoom_in(self):
+        " Zoom in the spatial domain image "
+
+        if self.spatial_array is None:
+            return
+
+        self.scale += 0.1
+        self.set_gray_image(self.spatial_array)
+
+    def image_zoom_out(self):
+        " Zoom out the spatial domain image "
+
+        if self.spatial_array is None:
+            return
+
+        self.scale -= 0.1
+        self.set_gray_image(self.spatial_array)
 
 
 if __name__ == '__main__':
