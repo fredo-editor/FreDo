5fb032540e7dd6af302fdc8419cd80febb64df62
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2015-07-19 22:02:44 +0530

    added files for setup and pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='FreDo Editor',
+      version='0.1',
+      description='Frequency Domain Image Editor',
+      author='Vighnesh Birodkar',
+      author_email='vighneshbirodkar@nyu.edu',
+      )
