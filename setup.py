975e5e59725f95cf7b59cb56833d29395311cf6b
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2015-07-21 03:01:51 +0530

    made setup.py support install_requires

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.xreadlines() if l]
+
 setup(name='FreDo Editor',
       version='0.1',
       description='Frequency Domain Image Editor',
       author='Vighnesh Birodkar',
       packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
       author_email='vighneshbirodkar@nyu.edu',
       entry_points={
-      'gui_scripts': [
-          'fredo = fredo.editor.main:run',
-      ]
+          'gui_scripts': ['fredo = fredo.editor.main:run']
       }
       )
