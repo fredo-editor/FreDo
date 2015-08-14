54d72f6006455205f4959f6df484ba4d2112fa62
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2015-08-15 02:03:17 +0530

    Added the reference to skimage liscence

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# Parts of this file were derived from the setup.py file of scikit-image
+# scikit-image liscnece can be found at
+# https://github.com/scikit-image/scikit-image/blob/master/LICENSE.txt
+
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
